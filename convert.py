eb2cb1c00ba3c4215ad27910f8be099107114a95
Lazarev Ivan <ilazarev@yandex-team.ru>
2011-03-18 11:33:17 +0000

    add converter source

---
+++ convert.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+import sys
+import json
+
+def utf8escape(str):
+    ret = '';
+    for char in str:
+   	ret += '&#' + hex(ord(char))[1:] + ';'
+    return ret
+
+def process(nodes):
+    ret = ''
+    for node in nodes:
+    	ret += '<node text="%s">' % utf8escape(node['title'])
+    	if node['children']:
+        	ret += process(node['children'])
+    	ret += '</node>'
+    return ret
+
+mindfile = file(sys.argv[1], 'rb').read()
+data = json.loads(mindfile)
+
+export = '<map version="0.9.0">'
+export += process([data['root']])
+export += '</map>'
+
+exportfile = open(sys.argv[2], 'wb+')
+exportfile.write(export.encode('utf-8'))
+
